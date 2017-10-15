76571efc7edfc57b2f51d836a01d3adb231c8e30
tushank <tushank.shethe@gmail.com>
2017-10-16 00:50:27 +0530

    + Added Card & card section

---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,49 @@
+//Import libraries for making a component
+import React from 'react';
+import { Text, View, Image } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
+
+//making a component
+//could also do that ({album: { title, artist, thumbnail_image } })
+const AlbumDetail = ({ album }) => {
+    const { title, artist, thumbnail_image } = album;
+    const { thumbnailStyle, headerContentStyle, thumbnailContainerStyle } = styles;
+
+    return (
+        <Card>
+            <CardSection>
+                <View style={thumbnailContainerStyle}>
+                    <Image
+                     style={thumbnailStyle}
+                     source={{ uri: thumbnail_image }}
+                    />
+                </View>
+                <View style={headerContentStyle}>
+                    <Text>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>                
+            </CardSection>
+        </Card>
+    );
+};
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    }
+};
+
+//making component available to other parts of the app
+export default AlbumDetail;
--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,23 +1,28 @@
 //Import libraries for making a component
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 //making a component
 class AlbumList extends Component {
 
-    state = { album: [] };
+    state = { albums: [] };
 
     componentWillMount() {
         axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-            .then(response => this.setState({ album: response.data }));
+            .then(response => this.setState({ albums: response.data }));
+    }
+
+    renderAlbums() {
+        return this.state.albums.map(album => <AlbumDetail key={album.title} album={album} />);
     }
 
     render() {
         console.log(this.state);
         return (
             <View>
-                <Text>Album List!!!</Text>
+                {this.renderAlbums()}
             </View>
         );
     }
